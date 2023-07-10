d31c136b2718457a412d9096293fad4509951727
steffijoji <steffijoji@gmail.com>
2023-07-10 14:00:49 -0300

    feat: add utility to generate random emails

--- src/test/java/support/Utils.java
+++ src/test/java/support/Utils.java
@@ -5,10 +5,24 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import runner.RunCucumberTest;
 
+import java.util.Random;
+
 public class Utils extends RunCucumberTest {
 
     public void waitUntilElementAppears(By element, int time) {
         WebDriverWait wait = new WebDriverWait(driver, time);
         wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public String getRandomEmail() {
+        String email_init = "qazando_";
+        String email_final = "@email.com.br";
+
+        Random random = new Random();
+        int min = 1;
+        int max = 999999999;
+        int result = random.nextInt(max-min) + min;
+
+        return email_init + result + email_final;
     }
 }
